29521e74eae4a2a782220c2fb41602069ff6b5d1
nhanquatot <nhanquatot@gmail.com>
2025-09-24 13:13:01 +0700

    Update route.ts

--- src/app/api/fetch-m3u8/route.ts
+++ src/app/api/fetch-m3u8/route.ts
@@ -1,5 +1,4 @@
-export const runtime = 'edge';  
-import { NextRequest, NextResponse } from 'next/server';
+ import { NextRequest, NextResponse } from 'next/server';
 import axios from 'axios';
 
 export async function POST(req: NextRequest) {
